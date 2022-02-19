09d2de6c0c15170bcfefa95e985a4c5d47554a30
Pratheekshap29 <68225339+Pratheekshap29@users.noreply.github.com>
2022-02-19 17:09:33 +0530

    some features added

--- main.js
+++ main.js
@@ -27,9 +27,7 @@ class UI{
 
         books.forEach((book)=>UI.addBooks(book));
     }
-
     
-
     static addBooks(book)
     {
         
@@ -38,16 +36,68 @@ class UI{
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
-        <td><a href=# class="badge rounded-pill bg-danger">X</a></td>
+        <td><a href=# class="badge rounded-pill bg-danger delete">X</a></td>
         `;
         console.log(tr);
-        document.querySelector('.t-body').appendChild(tr);
+        document.querySelector('#t-body').appendChild(tr);
         
     }
-    static deleteBook()
+    static deleteBook(el)
     {
+        if(confirm('Are you sure you want to delete?'))
+        {
+            el.remove();
+            UI.addMessage('The book details got deleted!','danger');
+        }   
+    }
+    static removeAll()
+    {
+        document.getElementById('title').value='';
+        document.getElementById('author').value='';
+        document.getElementById('isbn').value='';
 
+    }
+    static addMessage(msg,type){
+        const div=document.createElement('div');
+        div.className=`bg-${type} text-white p-3 mb-4`;
+        div.appendChild(document.createTextNode(msg));
+        const container=document.querySelector('.container');
+        const form=document.getElementById('my-form');
+        container.insertBefore(div,form);
+
+        setTimeout(()=>{
+            div.remove();
+        },3000)
     }
 }
 
+//display
 document.addEventListener('DOMContentLoaded',UI.displayBooks);
+
+//add book
+document.querySelector('#my-form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const title=document.getElementById('title').value;
+    const author=document.getElementById('author').value;
+    const isbn=document.getElementById('isbn').value;
+    if(title==='' || author==='' || isbn==='')
+    {
+        UI.addMessage('Enter all the details!','info')
+    }
+    else
+    {
+        const book=new Book(title,author,isbn);
+        UI.addBooks(book);
+        UI.addMessage('The book was successfully added!','info');
+        UI.removeAll();
+    }
+});
+
+document.getElementById('t-body').addEventListener('click',(e)=>{
+    if(e.target.classList.contains('delete'))
+    {
+        UI.deleteBook(e.target.parentElement.parentElement);
+    }
+
+})
+
